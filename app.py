85dec5e0c3d7e1954a8d41d00879ffab3e328240
santhoshgo28 <santhoshdevaraj2003@gmail.com>
2026-03-25 16:19:33 +0530

    Add 'K Suresh' to allowed employees list

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ EXCEL_FILE  = os.path.join(BASE_DIR, "questions ().xlsx")
 RESULT_FILE = os.path.join(BASE_DIR, "result.xlsx")
 
 ALLOWED_EMPLOYEES = [
-    "Santhosh 1",
+    "K Suresh",
     "Pavithra",
     "Ram",
     "janani G Hegde3",
